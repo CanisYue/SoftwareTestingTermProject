0820b9db77c881d4c9f44dbba54636100078be3c
Zirui Tang <ztang@mario.ece.utexas.edu>
2022-11-11 23:08:40 -0600

    update

--- Real_World_Projects/Jlint/tomcat/scripts/test.sh
+++ Real_World_Projects/Jlint/tomcat/scripts/test.sh
@@ -3,6 +3,19 @@ output_dir=/home/ecelrc/students/ztang/22f/SoftwareTestingTermProject/Real_World
 src_dir=/misc/scratch/zttest/apache-tomcat-10.0.27
 
 cd ${src_dir}
+if [ ! -d "${cur_dir}/jar_dir" ]
+then
+    mkdir jar_dir
+else
+    rm -rf jar_dir
+    mkdir jar_dir
+fi
+
+cp ${src_dir}/bin/*.jar ./jar_dir/
+cp ${src_dir}/lib/*.jar ./jar_dir/
+
+cd jar_dir
+
 if [ ! -d "${cur_dir}/class_dir" ]
 then
     mkdir class_dir
@@ -11,5 +24,17 @@ else
     mkdir class_dir
 fi
 
-cp ${src_dir}/bin/*.jar ./class_dir/
-cp ${src_dir}/libs/*.jar ./class_dir/
+for j in `ls`
+do
+    jar xvf ${j}
+done
+
+
+for cl in `find $PWD -name "*.class" | xargs ls -d`
+do
+    cp ${cl} class_dir/
+done
+ls class_dir/*.class
+cd class_dir
+
+/home/ecelrc/students/ztang/jlint/jlint-3.1.2/bin/jlint -inheritance -data_flow ./*.class &> ${output_dir}/result/tomcat.log
